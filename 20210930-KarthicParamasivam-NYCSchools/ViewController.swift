4a971322b6ff624ec5ef0eff1ef9fcdfe68d4fcc
Karthiccc <karthiccc23@gmail.com>
2021-10-01 00:46:05 -0500

    Initial Commit

---
+++ 20210930-KarthicParamasivam-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20210930-KarthicParamasivam-NYCSchools
+//
+//  Created by Karthic Paramasivam on 10/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
