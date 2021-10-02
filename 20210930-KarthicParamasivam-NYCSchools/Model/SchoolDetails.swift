7af5118f2cf59a28eebf36ed6deefb22df47bb85
Karthiccc <karthiccc23@gmail.com>
2021-10-02 00:23:41 -0500

    cleanup

--- 20210930-KarthicParamasivam-NYCSchools/Model/SchoolDetails.swift
+++ 20210930-KarthicParamasivam-NYCSchools/Model/SchoolDetails.swift
@@ -23,14 +23,4 @@ struct SchoolDetails: Codable {
         case satMathScore = "sat_math_avg_score"
         case satWritingScore = "sat_writing_avg_score"
     }
-//    
-//    init(from decoder: Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-//        dbn = try values.decode(String.self, forKey: .dbn)
-//        name = try values.decode(String.self, forKey: .name)
-//        numOfTestTakers = try values.decode(String.self, forKey: .numOfTestTakers)
-//        satReadingScore = try values.decode(String.self, forKey: .satReadingScore)
-//        satMathScore = try values.decode(String.self, forKey: .satMathScore)
-//        satWritingScore = try values.decode(String.self, forKey: .satWritingScore)
-//    }
 }
